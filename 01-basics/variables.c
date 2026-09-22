ff4f334c88bed7ea774e4f065d6524c58ab33753
Trần Ngọc Minh Khôi <trankhoi0606@gmail.com>
2026-09-22 16:19:53 +0700

    Add variables example

---
+++ 01-basics/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 20;
+    double gpa = 3.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("GPA: %.2f\n", gpa);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
